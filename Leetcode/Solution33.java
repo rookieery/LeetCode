f4cdabba3fce9b9f6f6e9b11fbdccdf1645315e6
rookieery <49903818+rookieery@users.noreply.github.com>
2019-06-12 14:41:28 +0800

    二分更新

--- Leetcode/Solution33.java
+++ Leetcode/Solution33.java
@@ -11,13 +11,16 @@ public class Solution33 {
      */
     //方法一：二分+递归
     public int search1(int[] nums, int target) {
-        return find(nums,0,nums.length - 1,target);
+        return find(nums, 0, nums.length - 1, target);
     }
-    private int find(int[] nums,int left ,int right ,int target) {
+
+    private int find(int[] nums, int left, int right, int target) {
+        //递归的终止条件
         if (left > right) {
             return -1;
         }
-        int mid = (left + right)/2;
+        //判断边界条件
+        int mid = left + (right - left) / 2;
         if (target == nums[left]) {
             return left;
         }
@@ -30,40 +33,38 @@ public class Solution33 {
         if (target > nums[right] && target < nums[left]) {
             return -1;
         }
+        //判断的核心代码
         if (nums[mid] < nums[right]) {
             if (target > nums[mid] && target < nums[right]) {
-                return binarySearch(nums,mid + 1,right - 1,target);
+                return binarySearch(nums, mid + 1, right - 1, target);
+            } else {
+                return find(nums, left, mid - 1, target);
             }
-            else {
-                return find(nums,left,mid - 1,target);
-            }
-        }
-        else {
+        } else {
             if (target > nums[left] && target < nums[mid]) {
-                return binarySearch(nums,left + 1,mid - 1,target);
-            }
-            else {
-                return find(nums,mid + 1,right,target);
+                return binarySearch(nums, left + 1, mid - 1, target);
+            } else {
+                return find(nums, mid + 1, right, target);
             }
         }
     }
-    private int binarySearch(int[] nums,int left,int right,int target) {
+
+    private int binarySearch(int[] nums, int left, int right, int target) {
         while (left <= right) {
-            int mid = (left + right)/2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] > target) {
                 right = mid - 1;
-            }
-            else if (nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
-            }
-            else {
+            } else {
                 return mid;
             }
         }
-        return  -1;
+        return -1;
     }
+
     //方法二：二分+二分
-    public int search(int[] nums, int target) {
+    public int search2(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
         }
@@ -81,20 +82,21 @@ public class Solution33 {
     }
 
     private int findPartition(int[] nums) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (mid != r  && nums[mid] > nums[mid + 1]) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            //此处是通过旋转点特殊的性质来得到旋转点
+            if (mid != right && nums[mid] > nums[mid + 1]) {
                 return mid;
             }
-            if (mid != l && nums[mid] < nums[mid - 1]) {
+            if (mid != left && nums[mid] < nums[mid - 1]) {
                 return mid - 1;
             }
-            if (nums[mid] < nums[l]) {
-                r = mid - 1;
+            if (nums[mid] < nums[left]) {
+                right = mid - 1;
             } else {
-                l = mid + 1;
+                left = mid + 1;
             }
         }
         return -1;
--- Leetcode/Solution81.java
+++ Leetcode/Solution81.java
@@ -8,19 +8,20 @@ public class Solution81 {
      * 编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。
      */
     public boolean search1(int[] nums, int target) {
-        return find(nums,0,nums.length - 1,target);
+        return find(nums, 0, nums.length - 1, target);
     }
-    private boolean find(int[] nums,int left ,int right ,int target) {
+
+    private boolean find(int[] nums, int left, int right, int target) {
         if (left > right) {
             return false;
         }
-        while(left < right && nums[left] == nums[left + 1]){
+        while (left < right && nums[left] == nums[left + 1]) {
             left++;
         }
-        while(left < right && nums[right] == nums[right - 1]){
+        while (left < right && nums[right] == nums[right - 1]) {
             right--;
         }
-        int mid = (left + right)/2;
+        int mid = left + (right - left) / 2;
         if (target == nums[left]) {
             return true;
         }
@@ -35,36 +36,33 @@ public class Solution81 {
         }
         if (nums[mid] < nums[right]) {
             if (target > nums[mid] && target < nums[right]) {
-                return binarySearch(nums,mid + 1,right - 1,target);
+                return binarySearch(nums, mid + 1, right - 1, target);
+            } else {
+                return find(nums, left, mid - 1, target);
             }
-            else {
-                return find(nums,left,mid - 1,target);
-            }
-        }
-        else {
+        } else {
             if (target > nums[left] && target < nums[mid]) {
-                return binarySearch(nums,left + 1,mid - 1,target);
-            }
-            else {
-                return find(nums,mid + 1,right,target);
+                return binarySearch(nums, left + 1, mid - 1, target);
+            } else {
+                return find(nums, mid + 1, right, target);
             }
         }
     }
-    private boolean binarySearch(int[] nums,int left,int right,int target) {
+
+    private boolean binarySearch(int[] nums, int left, int right, int target) {
         while (left <= right) {
-            int mid = (left + right)/2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] > target) {
                 right = mid - 1;
-            }
-            else if (nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
-            }
-            else {
+            } else {
                 return true;
             }
         }
-        return  false;
+        return false;
     }
+
     public boolean search2(int[] nums, int target) {
         for (int num : nums) {
             if (num == target) {
