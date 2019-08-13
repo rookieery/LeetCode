5258e649ac8c68841941d4f81d50a91d863254f1
rookieery <49903818+rookieery@users.noreply.github.com>
2019-08-14 07:35:11 +0800

    数组中的第K个最大元素

---
+++ Leetcode/Solution215.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class Solution215 {
+    /**
+     * 215. 数组中的第K个最大元素
+     * 在未排序的数组中找到第 k 个最大的元素。
+     * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+     */
+    //快排
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    //计数排序
+    public int findKthLargest2(int[] nums, int k) {
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int i : nums) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
+        int n = max - min;
+        int[] bucket = new int[n + 1];
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = nums[i] - min;
+            bucket[tmp]++;
+        }
+        for (int i = n; i >= 0; i--) {
+            if (bucket[i] > 0)
+                k -= bucket[i];
+            if (k <= 0)
+                return i + min;
+        }
+        return 0;
+    }
+}
