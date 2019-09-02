f85fa335dc70c2c49fe178c0dcf7097b0de909a1
rookieery <49903818+rookieery@users.noreply.github.com>
2019-09-02 09:42:59 +0800

    新增滑动窗口解法

--- Leetcode/Solution3.java
+++ Leetcode/Solution3.java
@@ -1,7 +1,11 @@
 package LeetCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution3 {
-    /**无重复字符的最长字串
+    /**
+     * 无重复字符的最长字串
      * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
      */
     public int lengthOfLongestSubstring(String s) {
@@ -26,8 +30,39 @@ public class Solution3 {
             if (a == 0) {
                 ch[last++] = s.charAt(i);
             }
-            max = Math.max(max,last - front);
+            max = Math.max(max, last - front);
         }
         return max;
+    }
+
+    //滑动窗口
+    public int lengthOfLongestSubstring1(String s) {
+        Set<Character> set = new HashSet<>();
+        int maxlen = 0;
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                maxlen = Math.max(maxlen, j - i);
+            } else {
+                //此操作会一直持续到移除掉s.charAt(j++)为止
+                set.remove(s.charAt(i++));
+            }
+        }
+        return maxlen;
+    }
+
+    //滑动窗口（优化）
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
     }
 }
