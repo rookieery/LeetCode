653507be5a661620681a32ca5ed5bd793064fa95
rookieery <49903818+rookieery@users.noreply.github.com>
2019-09-03 15:39:18 +0800

    新增排序

--- Leetcode/Solution128.java
+++ Leetcode/Solution128.java
@@ -1,9 +1,6 @@
 package LeetCode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Solution128 {
     /**
@@ -39,6 +36,29 @@ public class Solution128 {
         }
         return maxLength;
     }
+
+    //排序
+    public int longestConsecutive3(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int longestStreak = 1;
+        int currentStreak = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    currentStreak += 1;
+                } else {
+                    longestStreak = Math.max(longestStreak, currentStreak);
+                    currentStreak = 1;
+                }
+            }
+        }
+
+        return Math.max(longestStreak, currentStreak);
+    }
+
     //Set集合
     public int longestConsecutive1(int[] nums) {
         int maxLength = 0;
@@ -60,6 +80,7 @@ public class Solution128 {
         }
         return maxLength;
     }
+
     //Map集合
     //put和contain都不需要时间复杂度？
     public int longestConsecutive2(int[] nums) {
