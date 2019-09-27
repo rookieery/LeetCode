807bdf490d149936c642be3479137ec5ca729b2e
rookieery <49903818+rookieery@users.noreply.github.com>
2019-09-27 14:58:01 +0800

    直线上的最近距离

---
+++ Leetcode/Solution613.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class Solution613 {
+    /**
+     * 613. 直线上的最近距离
+     * 写一个查询语句，找到这些点中最近两个点之间的距离。
+     */
+    //聚合函数
+    /**
+     * select
+     * min(abs(p1.x-p2.x)) shortest
+     * from
+     * point p1,point p2
+     * where
+     * p1.x != p2.x;
+     */
+}
