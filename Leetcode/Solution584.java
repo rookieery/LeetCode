d88f4a3f8dad6c4bb1a4c60a05df29d60b9e767a
rookieery <49903818+rookieery@users.noreply.github.com>
2019-09-28 09:20:11 +0800

    寻找用户推荐人

---
+++ Leetcode/Solution584.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class Solution584 {
+    /**
+     * 584. 寻找用户推荐人
+     * 写一个查询语句，返回一个编号列表，列表中编号的推荐人的编号都 不是 2。
+     */
+    //ifnull函数使用
+    /**
+     * select
+     * name
+     * from
+     * customer
+     * where
+     * ifnull(referee_id,0) != 2;
+     */
+}
